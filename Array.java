0e7ec74a90890ae35b23de6da9cd13dbd4cf216f
Keston P. Banda <95207961+pachalobanda@users.noreply.github.com>
2022-02-02 16:48:48 -0800

    This being my second commit

--- Array.java
+++ Array.java
@@ -1,21 +1,33 @@
-
-	// Java program to demonstrate an array
-// of objects is declared with initial values.
-public class Array{
-
-public void Array() {
-
-
-		// Creating an array of objects
-		// declared with initial values
-		Object[] javaObjectArray
-			= { "Keston", new Integer(), "Pachalo",
-				new Integer() };
-
-	// Printing the values
-		System.out.println(javaObjectArray[0]);
-		System.out.println(javaObjectArray[1]);
-		System.out.println(javaObjectArray[2]);
-		System.out.println(javaObjectArray[3]);
-	  }
-	}
+class Main{
+   public static void main(String args[]){
+     //create array of student object  
+     Array[] obj = new Array[2] ;
+ 
+     //create actual student object
+     obj[0] = new Array();
+     obj[1] = new Array();
+ 
+     //assign data to student objects
+     obj[0].setData(100,"Keston");
+     obj[1].setData(200,"Pachalo");
+ 
+     //display the student object data
+     System.out.println("Student Object 1:");
+     obj[0].showData();
+     System.out.println("Student Object 2:");
+     obj[1].showData();
+  }
+}
+//Student class with StdAcc and Name as attributes
+class Array{
+    int StdAcc;
+    String Name;
+    public void setData(int c,String d){
+        StdAcc = c;
+        Name = d;
+     }
+    public void showData(){
+        System.out.print("Student Account = "+StdAcc + "  " + " Student Name = "+Name);
+        System.out.println();
+     }
+}
